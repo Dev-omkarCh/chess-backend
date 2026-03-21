a0f5379545981d377e444eabc5d0e5d677041e0a
Omkar <omkarchikhale2022@gmail.com>
2026-03-21 20:14:18 +0530

    production minor changes

--- config/cookieConfig.js
+++ config/cookieConfig.js
@@ -1,6 +1,5 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // Set secure flag in production
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    path: "/",
+    sameSite: "lax",
 };
