540e6814a777b859c4ef25f97223a24943988dec
Omkar <omkarchikhale2022@gmail.com>
2026-03-21 23:43:19 +0530

    local enviroment config

--- config/cookieConfig.js
+++ config/cookieConfig.js
@@ -1,5 +1,5 @@
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production", // Set secure flag in production
-    sameSite: "none",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Set SameSite to None in production for cross-site cookies
 };
